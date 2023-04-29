731681200f077f54f99d6a18dbe9cd7c2eeb39a1
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-04-29 13:47:36 +0530

    RESTful App

--- 15 REST/WTF is REST/script.js
+++ 15 REST/WTF is REST/script.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -10,24 +11,51 @@ app.use(express.json());
 const comments = [
     {
         username: 'Todd',
-        comment: 'lol this is so funny'
+        comment: 'lol this is so funny',
+        id: uuid()
     }, 
     {
         username: 'Skyler',
-        comment: 'I like to go birdwatching with my dog'
+        comment: 'I like to go birdwatching with my dog',
+        id: uuid()
     },
     {
         username: 'Sk8erBoi',
-        comment: 'tood plz delete your account -_-'
+        comment: 'tood plz delete your account -_- ',
+        id: uuid()
     },
     {
         username: 'onlysayswoof',
-        comment: 'woof woof woof'
+        comment: 'woof woof woof',
+        id: uuid()
     }
 ];
 
 app.get('/comments', (req, res) => {
     res.render('home', { comments });
+});
+
+app.get('/comments/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/comments', (req, res) => {
+    // console.log(req);
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() });
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = (req.params);
+    const comment = comments.find(c => c.id === id);
+    // console.log(comment);
+    res.render('comment', { ...comment });
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.body;
+    const c = comments.find(c => c.id === id);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000'));
