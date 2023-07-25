f8be1f44b2c7f5b25bf4de069e041459ee19809a
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-07-25 14:21:05 +0530

    Rename REST api folder

--- 06 Intermediate JS/01 Functions in JS/app.js
+++ 06 Intermediate JS/01 Functions in JS/app.js
@@ -1,38 +1,37 @@
 //FUNCTION OVERLOADING DOES NOT EXIST IN JAVASCRIPT!!!
 function sing() {
-    console.log("DO");   
-    console.log("RE");   
-    console.log("MI");   
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
 }
 
 // sing();
 
-//PASSING ARGUMENTS
+//PASSING
 
 function greet(person) {
-    console.log(`Hi ${person}!`);
+  console.log(`Hi ${person}!`);
 }
 
 // greet("Gurditt");
 
 function greet(firstName, lastName) {
-    console.log(`Hi ${firstName} ${lastName[0]}!`);
+  console.log(`Hi ${firstName} ${lastName[0]}!`);
 }
 
 // greet("George", "Clooney");
 
 function repeat(str, num) {
-
-    let result = "";
-    for(let i = 0; i < num; i++) result+=str;
-    console.log(result);
+  let result = "";
+  for (let i = 0; i < num; i++) result += str;
+  console.log(result);
 }
 
 //***************************************************//
 
-function add(x,y) {
-    if(typeof x !== 'number' || typeof y !== 'number') return "can't add";
+function add(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") return "can't add";
 
-    return x + y;
-    console.log("lol");
+  return x + y;
+  console.log("lol");
 }
