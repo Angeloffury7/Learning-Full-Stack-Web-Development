c41d3b5b821b1e6ef58b47d6b705a518db10d7e7
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-08-06 16:03:58 +0530

    state lifting continued and controlled components

--- 25 React/first-react-app/src/App.jsx
+++ 25 React/first-react-app/src/App.jsx
@@ -1,7 +1,7 @@
-import Video from "./components/Video";
-import PlayButton from "./components/PlayButton";
 import videoDB from "./data/data";
 import AddVideo from "./components/AddVideo";
+import VideoList from "./components/VideoList";
+
 import { useState } from "react";
 
 import "./App.css";
@@ -10,7 +10,7 @@ function App() {
     const [videos, setVideos] = useState(videoDB);
 
     function addVideo(video) {
-        setVideos([...videos, {...video, id: videos.length + 1}]); //generating id of new video
+        setVideos([...videos, { ...video, id: videos.length + 1 }]); //generating id of new video
     }
 
     console.log("Rendering App");
@@ -19,31 +19,8 @@ function App() {
         <div className="App" onClick={() => console.log("App clicked")}>
             <h2>Videos</h2>
 
-            {/* <AddVideo addVideo={setVideos} /> */}
             <AddVideo addVideo={addVideo} />
-
-            {videos.map((video) => (
-                <Video
-                    key={video.id}
-                    id={video.id}
-                    title={video.title}
-                    views={video.views}
-                    time={video.time}
-                    channel={video.channel}
-                    verified={video.verified}
-                >
-                    <PlayButton
-                        onSmash={() => console.log("playing ", video.title)}
-                        onPause={() => console.log("paused ", video.title)}
-                    >
-                        {video.title}
-                    </PlayButton>
-                    {/* Now we are nesting the playbutton inside the Video component as 
-                  we want a play/pause button with each video. Access it using
-                  children props. */}
-                </Video>
-            ))}
-
+            <VideoList videos={videos} />
             {/* <Counter /> */}
         </div>
     );
--- 25 React/first-react-app/src/components/AddVideo.jsx
+++ 25 React/first-react-app/src/components/AddVideo.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 import "./styles/AddVideo.css";
 
+const initialVideoState = {
+    time: "3 hours ago",
+    channel: "gurdittcancode",
+    verified: false,
+    title: "",
+    views: "",
+};
+
 function AddVideo({ addVideo }) {
-    const [video, setVideo] = useState({
-        time: "3 hours ago",
-        channel: "gurdittcancode",
-        verified: false,
-    });
+    const [video, setVideo] = useState(initialVideoState);
 
     function handleSubmit(evt) {
         evt.preventDefault();
-        console.log(video); //Getting the video object yayy
+        console.log(video);
         addVideo(video);
+
+        setVideo(initialVideoState); //setting the video state back to initial state
     }
 
     function handleChange(evt) {
-        //to pick up values
         evt.stopPropagation();
         // console.log(evt.target.value, evt.target.name);
 
         setVideo({ ...video, [evt.target.name]: evt.target.value });
-        // Adding square brackets as key must be a string, brackets tell JS that this expressions
-        //needs to be evaluated
+        // An object's key can't be a variable, so we are adding square brackets to tell JS to evaulate it first.
 
         // console.log(video);
     }
@@ -33,15 +37,15 @@ function AddVideo({ addVideo }) {
                 name="title"
                 onChange={handleChange}
                 placeholder="title"
+                value={video.title}
             />
             <input
                 type="text"
                 name="views"
                 onChange={handleChange}
                 placeholder="views"
+                value={video.views}
             />
-
-            {video.title}
 
             <button onClick={handleSubmit}>Add Video</button>
         </form>
